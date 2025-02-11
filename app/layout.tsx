0406f541f1c7df408b17fac6a149f282bb288a68
ph1234-Hawk <35072905+ph1234-dev@users.noreply.github.com>
2025-02-11 23:19:01 +0800

    next js files (before chapter6 tutorial from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function RootLayout({ children, }: {  children: React.ReactNode;}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
+      {/* <p className={`${lusitana.className} antialiased`}>Meow meow meow meow</p> */}
     </html>
   );
 }
